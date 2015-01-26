1846a6a4dfcb9da7c1e64aafb9760d638335c3e6
Nic Scott <nls.inbox@gmail.com>
2015-01-26 12:59:59 -0500

    Fixed

--- ad_user_query.sh
+++ ad_user_query.sh
@@ -10,17 +10,3 @@ read text
 
 #new command - filters OU's out
 dscl "/Active Directory/$domain_name/All Domains" -read /Users/$text dsAttrTypeNative:memberOf | awk '/dsAttr/{y=1;next}y' | cut -d "," -f 1 | cut -c 5-50
-
-
-=======
-#Variables
-domain_name="xxx"
-
-#User input
-echo "Enter A User Name: "
-read text
-
-#command - filters OU's out
-dscl "/Active Directory/$domain_name/All Domains" -read /Users/$text dsAttrTypeNative:memberOf | awk '/dsAttr/{y=1;next}y' | cut -d "," -f 1 | cut -c 5-50
-
->>>>>>> FETCH_HEAD
