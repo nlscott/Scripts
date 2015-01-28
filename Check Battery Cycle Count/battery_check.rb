227a5650245828e9013befea9e4116ef1ab55058
nscott <nscott@scott-mba.local>
2015-01-27 19:08:46 -0500

    Ruby: Check Current Battery Cycles on Mac Computers
    This script checks the current cycle, the maximum recommended count and
    reports back the status and how many cycles the machine has left.
    
    update: added colored text output

--- Check Battery Cycle Count/battery_check.rb
+++ Check Battery Cycle Count/battery_check.rb
@@ -6,31 +6,45 @@ current_cycle_count=`ioreg -w0 -l | grep -w CycleCount | awk '{print $5}' | awk 
 max_cycle_count=`ioreg -w0 -l | grep -w DesignCycleCount9C | awk '{print $5}' | awk 'NR==1'`
 cycle_check=`ioreg -w0 -l | grep -w CycleCount | awk '{print $3}' | awk 'NR==1' | cut -d '"' -f 2`
 
+#define colored text
+class String
+	def red; "\033[31m#{self}\033[0m" end
+	def green; "\033[32m#{self}\033[0m" end
+	def brown; "\033[33m#{self}\033[0m" end
+	def blue; "\033[34m#{self}\033[0m" end
+	def magenta; "\033[35m#{self}\033[0m" end
+	def cyan; "\033[36m#{self}\033[0m" end
+	def gray; "\033[37m#{self}\033[0m" end
+	def yellow; "\033[93m#{self}\033[0m" end
+end
+
 
 current=current_cycle_count.chop.to_i
 max=max_cycle_count.chop.to_i
 cycles_left=max-current
 
-puts "Current Battery Cycles: " + current.to_s
-puts "Maximum Battery Cycles: " + max.to_s
+puts "Current Battery Cycles: ".cyan + current.to_s.green
+puts "Maximum Battery Cycles: ".cyan + max.to_s.green
 
 
 
 if cycle_check.chop == "CycleCount"
 	if max - current > 0
-	puts "\nBattery Status: Good"
-	puts "You still have " + cycles_left.to_s + " cycles left"
+	puts "- - - - - - - - "
+	puts "Battery Status: ".cyan + "Good".green
+	puts ("You still have " + cycles_left.to_s + " cycles left").green
 
 
 	elsif max - current < 0
-	puts "\nBattery Status: Needs Replaced"
-	puts "You are " + cycles_left.to_s + " cycles over"
+	puts "- - - - - - - - "
+	puts "Battery Status: ".cyan + "Needs Replaced".red
+	puts ("You are " + cycles_left.to_s + " cycles over").red
 
 	else
-	puts "oops! something went wrong and the battery status could not be checked"
+	puts "oops! something went wrong and the battery status could not be checked".yellow
 		
 	end
 else
-	puts "Error: an internal battery could not be found."
+	puts "Error: an internal battery could not be found.".yellow
 end
 
