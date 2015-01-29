71f8ef86aa955cda3ce0a94fbaef5e5fb090bd8b
Nic Scott <nls.inbox@gmail.com>
2015-01-29 15:47:57 -0500

    Rename disable_auto_java_updates.sh to disable_java_autoupdates.sh

--- disable_auto_java_updates.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#disable auto Java updates
-sudo defaults write /Library/Preferences/com.oracle.java.Java-Updater JavaAutoUpdateEnabled -bool false
---
+++ disable_java_autoupdates.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#disable auto Java updates
+sudo defaults write /Library/Preferences/com.oracle.java.Java-Updater JavaAutoUpdateEnabled -bool false
