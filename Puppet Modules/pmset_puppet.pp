d8e3fa0d247972d759287c0ee66de74b2b60033c
Nic Scott <scottnl@K113382.local>
2016-07-26 15:31:44 -0400

    updated code
    Removed unnecessary “grep”

--- Puppet Modules/pmset_puppet.pp
+++ Puppet Modules/pmset_puppet.pp
@@ -1,5 +1,5 @@
 exec { "set_power_settings":
   		path   => ["/usr/bin", "/usr/sbin", "/bin", "/sbin"],
   		command => "pmset -c displaysleep 30 sleep 0 disksleep 0 womp 1 autorestart 1",
-		unless => "pmset -g | grep -w sleep | awk '{print $2}' | grep -w 0 >/dev/null"
+		unless => "pmset -g | awk '/ sleep/ && !/0/{print $2}' >dev/null"
 		}
