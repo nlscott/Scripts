ec41b77cd9d0ff40ff5be79d7c1e0b6b9ce801b5
nic scott <nls.inbox@gmail.com>
2022-10-05 18:55:29 -0700

    GAM update contact info
    use google gam to update personal contact info in  google account

---
+++ Google GAM/gam_update_contact_info.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+emailAccount="user.name@email.com"
+gamCommand="path_to_gam_command"
+
+$gamCommand update user $emailAccount emails home ""
+$gamCommand/gamCommand update user $emailAccount phone type home value "            " notprimary
