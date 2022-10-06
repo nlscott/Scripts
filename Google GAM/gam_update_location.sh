367525f4cc8039d5e6bee1191458df464df2a09e
nic scott <nls.inbox@gmail.com>
2022-10-05 18:55:56 -0700

    GAM update location

---
+++ Google GAM/gam_update_location.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+emailAccount="user.name@email.com"
+gamCommand="path_to_gam_command"
+
+$gamCommand update user $userEmail location floor "Floor Name" section "section" endlocation
+
+
+
+
+
